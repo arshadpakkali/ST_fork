578ea5e778999e310c249410ac50fe28881753ef
Arshad Pakkali <arshadpakkali@gmail.com>
2021-07-02 13:20:38 +0530

    added ligatures & color scheme

--- config.h
+++ config.h
@@ -95,31 +95,29 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+   /* 8 normal colors */
+  [0] = "#0c0d0e", /* black   */
+  [1] = "#e31a1c", /* red     */
+  [2] = "#31a354", /* green   */
+  [3] = "#dca060", /* yellow  */
+  [4] = "#3182bd", /* blue    */
+  [5] = "#756bb1", /* magenta */
+  [6] = "#80b1d3", /* cyan    */
+  [7] = "#b7b8b9", /* white   */
 
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+  /* 8 bright colors */
+  [8]  = "#737475", /* black   */
+  [9]  = "#e31a1c", /* red     */
+  [10] = "#31a354", /* green   */
+  [11] = "#dca060", /* yellow  */
+  [12] = "#3182bd", /* blue    */
+  [13] = "#756bb1", /* magenta */
+  [14] = "#80b1d3", /* cyan    */
+  [15] = "#fcfdfe", /* white   */
 
-	[255] = 0,
-
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
+  /* special colors */
+  [256] = "#0c0d0e", /* background */
+  [257] = "#b7b8b9", /* foreground */
 };
 
 
@@ -127,9 +125,9 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+static unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 257;
 
 /*
